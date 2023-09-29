abde0292b2552997b0035e96ba97c9ef2e83d405
huhouhuam@outlook.com <huhouhuam@outlook.com>
2023-09-29 23:04:42 +0800

    adjust the test class library directory structure

---
+++ test.ps1
@@ -0,0 +1,19 @@
+$dockerOSType = docker info --format "{{.OSType}}"
+if ($dockerOSType -ieq 'linux') {
+  & dotnet test --verbosity normal --settings coverletArgs.runsettings --no-build --no-restore
+} else {
+  & dotnet test --filter Category!=LinuxIntegration --verbosity normal --settings coverletArgs.runsettings --no-build --no-restore
+}
+
+$exitWithError = $LastExitCode -ne 0
+
+$openCoverFile = Get-ChildItem -Path "test/*/coverage.opencover.xml" -Recurse | Sort-Object LastWriteTime | Select-Object -last 1
+if (Test-Path "$openCoverFile") {
+  Write-Host "Uploading coverage file"
+  & codecov -f "$openCoverFile" -t bda1c835-c4a2-4a1a-8d38-999b9a8ea80b
+  $exitWithError = $exitWithError -or $LastExitCode -ne 0
+}
+
+if ($exitWithError) {
+  exit 1
+}
