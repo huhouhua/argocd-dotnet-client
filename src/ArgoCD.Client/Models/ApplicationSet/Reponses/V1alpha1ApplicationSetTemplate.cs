3b3184d739df6e598e08ec400878c22bc07b3c20
huhouhuam@outlook.com <huhouhuam@outlook.com>
2023-10-08 23:52:58 +0800

    defin reponses model for applicationset api

--- src/ArgoCD.Client/Models/ApplicationSet/Reponses/V1alpha1ApplicationSetTemplate.cs
+++ src/ArgoCD.Client/Models/ApplicationSet/Reponses/V1alpha1ApplicationSetTemplate.cs
@@ -1,13 +1,22 @@
 using System;
+using System.Buffers.Text;
 using System.Collections.Generic;
 using System.Text;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ArgoCD.Client.Models.ApplicationSet.Reponses
 {
     public class V1alpha1ApplicationSetTemplate
     {
+        /// <summary>
+        /// ApplicationSetTemplateMeta represents the Argo CD application fields that may
+        /// be used for Applications generated from the ApplicationSet(based on metav1.ObjectMeta)
+        /// </summary>
         public V1alpha1ApplicationSetTemplateMeta Metadata { get; set; }
 
-        public string Spec { get; set; }
+        /// <summary>
+        /// ApplicationSpec represents desired application state. Contains link to repository with application definition and additional parameters link definition revision.
+        /// </summary>
+        public V1alpha1ApplicationSpec Spec { get; set; }
     }
 }
