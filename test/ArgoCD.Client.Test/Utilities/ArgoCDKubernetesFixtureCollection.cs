1ddfdf75ecac49af9ceeb08a20a97c1e85967ff9
huhouhuam@outlook.com <huhouhuam@outlook.com>
2023-10-22 00:06:21 +0800

    add k8s integration test

---
+++ test/ArgoCD.Client.Test/Utilities/ArgoCDKubernetesFixtureCollection.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArgoCD.Client.Test.Utilities
+{
+    [CollectionDefinition("ArgoCDKubernetesFixture")]
+    public class ArgoCDKubernetesFixtureCollection : ICollectionFixture<ArgoCDKubernetesFixture>
+    {
+
+    }
+}
