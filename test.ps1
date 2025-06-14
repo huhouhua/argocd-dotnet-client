657b7a183eeb54fefb621fd8165bf4efecfc8984
huhouhua <huhouhuam@outlook.com>
2025-06-15 02:31:09 +0800

    fix: upload coverage reports to codecov

--- test.ps1
+++ test.ps1
@@ -1,3 +1,6 @@
+Get-ChildItem Env:
+Write-Host "API key is $ENV:CODECOV_TOKEN"
+
 $dockerOSType = docker info --format "{{.OSType}}"
 if ($dockerOSType -ieq 'linux') {
   & dotnet test  --verbosity normal --settings coverletArgs.runsettings --no-build   --no-restore
@@ -10,7 +13,7 @@ $exitWithError = $LastExitCode -ne 0
 $openCoverFile = Get-ChildItem -Path "test/*/coverage.opencover.xml" -Recurse | Sort-Object LastWriteTime | Select-Object -last 1
 if (Test-Path "$openCoverFile") {
   Write-Host "Uploading coverage file"
-  & codecov -f "$openCoverFile" -t bda1c835-c4a2-4a1a-8d38-999b9a8ea80b
+  & codecov -f "$openCoverFile" -t $ENV:CODECOV_TOKEN
   $exitWithError = $exitWithError -or $LastExitCode -ne 0
 }
 
