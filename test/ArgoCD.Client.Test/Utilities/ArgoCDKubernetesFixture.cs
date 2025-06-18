0abefdc10a3d1f9f85ea593aa2102dae241a8e25
huhouhua <huhouhuam@outlook.com>
2025-06-19 01:44:57 +0800

    feat: support dotnet 7.x/9.x

--- test/ArgoCD.Client.Test/Utilities/ArgoCDKubernetesFixture.cs
+++ test/ArgoCD.Client.Test/Utilities/ArgoCDKubernetesFixture.cs
@@ -45,7 +45,7 @@ namespace ArgoCD.Client.Test.Utilities
             await InitializeDataAsync();
         }
 
-        public  async  Task DisposeAsync()
+        public async Task DisposeAsync()
         {
             await UninstallAsync();
             kubernetes?.Dispose();
@@ -63,7 +63,14 @@ namespace ArgoCD.Client.Test.Utilities
             }
 
             kubernetes = new Kubernetes(config);
-            resourceList = await KubernetesYaml.LoadAllFromFileAsync($"{SolutionRootFolder}/{InstallFilePath}");
+
+#if NET5_0 || NETSTANDARD2_1
+            resourceList = await k8s.Yaml.LoadAllFromFileAsync($"{SolutionRootFolder}/{InstallFilePath}");
+#else
+                        resourceList = await KubernetesYaml.LoadAllFromFileAsync($"{SolutionRootFolder}/{InstallFilePath}");
+#endif
+
+            // Ensure this line is added at the top of the file with other using directives
             await RestartAsync();
         }
 
@@ -89,13 +96,13 @@ namespace ArgoCD.Client.Test.Utilities
 
         private async Task<string> FindPasswordAsync()
         {
-            var secretList = await kubernetes.CoreV1.ListSecretForAllNamespacesAsync(null, null,
-                $"metadata.name={AdminSecretName},metadata.namespace={NameSpace}");
-            var adminSecret = secretList.Items.FirstOrDefault(q => q.Name().Equals(AdminSecretName));
-            if (adminSecret == null)
-            {
-                throw new Exception($"{AdminSecretName} secret not found");
-            }
+#if NET5_0 || NETSTANDARD2_1
+            var secretList = await kubernetes.ListNamespacedSecretAsync(NameSpace, null, null, null, $"metadata.name={AdminSecretName}");
+#else
+  var secretList = await kubernetes.CoreV1.ListNamespacedSecretAsync(NameSpace, null, null, null, $"metadata.name={AdminSecretName}");
+#endif
+
+            var adminSecret = secretList.Items.FirstOrDefault(q => q.Metadata.Name.Equals(AdminSecretName)) ?? throw new Exception($"{AdminSecretName} secret not found");
 
             bool ok = adminSecret.Data.TryGetValue("password", out byte[] password);
             Assert.True(ok);
@@ -105,14 +112,12 @@ namespace ArgoCD.Client.Test.Utilities
 
         private async Task<int> FindPortAsync()
         {
-            var serviceList = await kubernetes.CoreV1.ListNamespacedServiceAsync(NameSpace, null, null,
-                $"metadata.name={ServerName},metadata.namespace={NameSpace}");
-            var service = serviceList.Items.FirstOrDefault(q => q.Name().Equals(ServerName));
-            if (service == null)
-            {
-                throw new Exception($"{ServerName} service not found");
-            }
-
+#if NET5_0 || NETSTANDARD2_1
+            var serviceList = await kubernetes.ListNamespacedServiceAsync(NameSpace, null, null, null, $"metadata.name={ServerName}");
+#else
+     var serviceList = await kubernetes.CoreV1.ListNamespacedServiceAsync(NameSpace, null, null, null, $"metadata.name={ServerName}");
+#endif
+            var service = serviceList.Items.FirstOrDefault(q => q.Metadata.Name.Equals(ServerName)) ?? throw new Exception($"{ServerName} service not found");
             Assert.Equal("NodePort", service.Spec.Type, true);
             return service.Spec.Ports.First(q => q.Name == "http").NodePort ?? default;
         }
@@ -171,6 +176,232 @@ namespace ArgoCD.Client.Test.Utilities
             }
         }
 
+#if NET5_0 || NETSTANDARD2_1
+        private async Task ApplyForKubernetesAsync(IKubernetesObject<V1ObjectMeta> objectMeta, object resource,
+       string fieldSelector)
+        {
+            switch (objectMeta.Kind)
+            {
+                case V1CustomResourceDefinition.KubeKind:
+                    var newDefinition = (V1CustomResourceDefinition)resource;
+
+                    var definitionList =
+                        await kubernetes.ListCustomResourceDefinitionAsync(null, null, fieldSelector,
+                            null);
+                    if (!definitionList.Items.Any())
+                    {
+                        _ = await kubernetes.CreateCustomResourceDefinitionAsync(newDefinition);
+                    }
+                    else
+                    {
+                        var oldDefinition = definitionList.Items.First();
+                        _ = await kubernetes.PatchCustomResourceDefinitionAsync(
+                            new V1Patch(CreatePathData(newDefinition, oldDefinition), V1Patch.PatchType.JsonPatch),
+                            newDefinition.Name());
+                    }
+
+                    break;
+                case V1ServiceAccount.KubeKind:
+                    var newServiceAccount = (V1ServiceAccount)resource;
+
+                    var serviceAccountList =
+                        await kubernetes.ListNamespacedServiceAccountAsync(NameSpace, null, null, fieldSelector);
+                    if (!serviceAccountList.Items.Any())
+                    {
+                        _ = await kubernetes.CreateNamespacedServiceAccountAsync(newServiceAccount, NameSpace);
+                    }
+                    else
+                    {
+                        var oldServiceAccount = serviceAccountList.Items.First();
+                        _ = await kubernetes.PatchNamespacedServiceAccountAsync(
+                            new V1Patch(CreatePathData(newServiceAccount, oldServiceAccount),
+                                V1Patch.PatchType.JsonPatch), oldServiceAccount.Name(), oldServiceAccount.Namespace());
+                    }
+
+                    break;
+                case V1Role.KubeKind:
+                    var newRole = (V1Role)resource;
+
+                    var roleList =
+                        await kubernetes.ListNamespacedRoleAsync(NameSpace, null, null,
+                            fieldSelector);
+                    if (!roleList.Items.Any())
+                    {
+                        _ = await kubernetes.CreateNamespacedRoleAsync(newRole, NameSpace);
+                    }
+                    else
+                    {
+                        var oldRole = roleList.Items.First();
+                        _ = await kubernetes.PatchNamespacedRoleAsync(
+                            new V1Patch(CreatePathData(newRole, oldRole), V1Patch.PatchType.JsonPatch), oldRole.Name(),
+                            oldRole.Namespace());
+                    }
+
+                    break;
+                case V1ClusterRole.KubeKind:
+                    var newClusterRole = (V1ClusterRole)resource;
+
+                    var clusterRoleList =
+                        await kubernetes.ListClusterRoleAsync(null, null, fieldSelector);
+                    if (!clusterRoleList.Items.Any())
+                    {
+                        _ = await kubernetes.CreateClusterRoleAsync(newClusterRole);
+                    }
+                    else
+                    {
+                        var oldClusterRole = clusterRoleList.Items.First();
+                        _ = await kubernetes.PatchClusterRoleAsync(
+                            new V1Patch(CreatePathData(newClusterRole, oldClusterRole), V1Patch.PatchType.JsonPatch),
+                            oldClusterRole.Name());
+                    }
+
+                    break;
+                case V1RoleBinding.KubeKind:
+                    var newRoleBinding = (V1RoleBinding)resource;
+
+                    var roleBindingList =
+                        await kubernetes.ListRoleBindingForAllNamespacesAsync(null, null,
+                            fieldSelector);
+                    if (!roleBindingList.Items.Any())
+                    {
+                        _ = await kubernetes.CreateNamespacedRoleBindingAsync(newRoleBinding,
+                            NameSpace);
+                    }
+                    else
+                    {
+                        var oldRoleBinding = roleBindingList.Items.First();
+                        _ = await kubernetes.PatchNamespacedRoleBindingAsync(
+                            new V1Patch(CreatePathData(newRoleBinding, oldRoleBinding), V1Patch.PatchType.JsonPatch),
+                            oldRoleBinding.Name(), oldRoleBinding.Namespace());
+                    }
+
+                    break;
+                case V1ClusterRoleBinding.KubeKind:
+                    var newClusterRoleBinding = (V1ClusterRoleBinding)resource;
+
+                    var clusterRoleBindingList =
+                        await kubernetes.ListClusterRoleBindingAsync(null, null, fieldSelector);
+                    if (!clusterRoleBindingList.Items.Any())
+                    {
+                        _ = await kubernetes.CreateClusterRoleBindingAsync(newClusterRoleBinding);
+                    }
+                    else
+                    {
+                        var oldClusterRoleBinding = clusterRoleBindingList.Items.First();
+                        _ = await kubernetes.PatchClusterRoleBindingAsync(
+                            new V1Patch(CreatePathData(newClusterRoleBinding, oldClusterRoleBinding),
+                                V1Patch.PatchType.JsonPatch), oldClusterRoleBinding.Name());
+                    }
+
+                    break;
+                case V1ConfigMap.KubeKind:
+                    var newConfigMap = (V1ConfigMap)resource;
+
+                    var configMapList =
+                        await kubernetes.ListConfigMapForAllNamespacesAsync(null, null, fieldSelector);
+                    if (!configMapList.Items.Any())
+                    {
+                        _ = await kubernetes.CreateNamespacedConfigMapAsync(newConfigMap, NameSpace);
+                    }
+                    else
+                    {
+                        var oldConfigMap = configMapList.Items.First();
+                        _ = await kubernetes.PatchNamespacedConfigMapAsync(
+                            new V1Patch(CreatePathData(newConfigMap, oldConfigMap), V1Patch.PatchType.JsonPatch),
+                            oldConfigMap.Name(), oldConfigMap.Namespace());
+                    }
+
+                    break;
+                case V1Secret.KubeKind:
+                    var newSecret = (V1Secret)resource;
+                    var secretList = await kubernetes.ListSecretForAllNamespacesAsync(null, null, fieldSelector);
+                    if (!secretList.Items.Any())
+                    {
+                        _ = await kubernetes.CreateNamespacedSecretAsync(newSecret, NameSpace);
+                    }
+                    else
+                    {
+                        var oldSecret = secretList.Items.First();
+                        _ = await kubernetes.PatchNamespacedSecretAsync(
+                            new V1Patch(CreatePathData(newSecret, oldSecret), V1Patch.PatchType.JsonPatch),
+                            oldSecret.Name(), oldSecret.Namespace());
+                    }
+
+                    break;
+                case V1Service.KubeKind:
+                    var newService = (V1Service)resource;
+                    var serviceList =
+                        await kubernetes.ListNamespacedServiceAsync(NameSpace, null, null, fieldSelector);
+                    if (!serviceList.Items.Any())
+                    {
+                        _ = await kubernetes.CreateNamespacedServiceAsync(newService, NameSpace);
+                    }
+                    else
+                    {
+                        var oldService = serviceList.Items.First();
+                        _ = await kubernetes.PatchNamespacedServiceAsync(
+                            new V1Patch(CreatePathData(newService, oldService), V1Patch.PatchType.JsonPatch),
+                            oldService.Name(), oldService.Namespace());
+                    }
+
+                    break;
+                case V1Deployment.KubeKind:
+                    var newDeployment = (V1Deployment)resource;
+                    var deploymentList =
+                        await kubernetes.ListDeploymentForAllNamespacesAsync(null, null, fieldSelector);
+                    if (!deploymentList.Items.Any())
+                    {
+                        _ = await kubernetes.CreateNamespacedDeploymentAsync(newDeployment, NameSpace);
+                    }
+                    else
+                    {
+                        var oldDeployment = deploymentList.Items.First();
+                        _ = await kubernetes.PatchNamespacedDeploymentAsync(
+                            new V1Patch(CreatePathData(newDeployment, oldDeployment), V1Patch.PatchType.JsonPatch),
+                            oldDeployment.Name(), oldDeployment.Namespace());
+                    }
+
+                    break;
+                case V1StatefulSet.KubeKind:
+                    var newStatefulSet = (V1StatefulSet)resource;
+                    var statefulSetList =
+                        await kubernetes.ListStatefulSetForAllNamespacesAsync(null, null, fieldSelector);
+                    if (!statefulSetList.Items.Any())
+                    {
+                        _ = await kubernetes.CreateNamespacedStatefulSetAsync(newStatefulSet, NameSpace);
+                    }
+                    else
+                    {
+                        var oldStatefulSet = statefulSetList.Items.First();
+                        _ = await kubernetes.PatchNamespacedStatefulSetAsync(
+                            new V1Patch(CreatePathData(newStatefulSet, oldStatefulSet), V1Patch.PatchType.JsonPatch),
+                            oldStatefulSet.Name(), oldStatefulSet.Namespace());
+                    }
+
+                    break;
+                case V1NetworkPolicy.KubeKind:
+                    var newNetworkPolicy = (V1NetworkPolicy)resource;
+                    var networkPolicyList =
+                        await kubernetes.ListNetworkPolicyForAllNamespacesAsync(null, null, fieldSelector);
+                    if (!networkPolicyList.Items.Any())
+                    {
+                        _ = await kubernetes.CreateNamespacedNetworkPolicyAsync(newNetworkPolicy,
+                            NameSpace);
+                    }
+                    else
+                    {
+                        var oldNetworkPolicy = networkPolicyList.Items.First();
+                        _ = await kubernetes.PatchNamespacedNetworkPolicyAsync(
+                            new V1Patch(CreatePathData(newNetworkPolicy, oldNetworkPolicy),
+                                V1Patch.PatchType.JsonPatch), oldNetworkPolicy.Name(), oldNetworkPolicy.Namespace());
+                    }
+
+                    break;
+                default:
+                    throw new NotImplementedException($"{objectMeta.Kind} not implemented!");
+            }
+        }
+#else
         private async Task ApplyForKubernetesAsync(IKubernetesObject<V1ObjectMeta> objectMeta, object resource,
             string fieldSelector)
         {
@@ -395,7 +626,7 @@ namespace ArgoCD.Client.Test.Utilities
                     throw new NotImplementedException($"{objectMeta.Kind} not implemented!");
             }
         }
-
+#endif
         private async Task UninstallAsync()
         {
             foreach (object item in resourceList)
@@ -410,11 +641,19 @@ namespace ArgoCD.Client.Test.Utilities
 
             V1Namespace GetNamespace()
             {
-                V1NamespaceList nameSpaceList = kubernetes.CoreV1.ListNamespace(
+#if NET5_0 || NETSTANDARD2_1
+                V1NamespaceList nameSpaceList = kubernetes.ListNamespace(
+                    pretty: "false",
+                    allowWatchBookmarks: null,
+                    fieldSelector: fieldSelector
+                );
+#else
+    V1NamespaceList nameSpaceList = kubernetes.CoreV1.ListNamespace(
                     pretty: false,
                     allowWatchBookmarks: null,
                     fieldSelector: fieldSelector
                 );
+#endif
                 return nameSpaceList.Items.FirstOrDefault();
             }
 
@@ -425,12 +664,19 @@ namespace ArgoCD.Client.Test.Utilities
                 {
                     return;
                 }
-                await kubernetes.CoreV1.DeleteNamespaceAsync(NameSpace);
+#if NET5_0 || NETSTANDARD2_1
+                await kubernetes.DeleteNamespaceAsync(NameSpace);
+
+                var watch = await kubernetes.ListNamespaceWithHttpMessagesAsync(
+                    watch: true,
+                    fieldSelector: fieldSelector);
+#else
+       await kubernetes.CoreV1.DeleteNamespaceAsync(NameSpace);
 
                 var watch = await kubernetes.CoreV1.ListNamespaceWithHttpMessagesAsync(
                     watch: true,
                     fieldSelector: fieldSelector);
-
+#endif
                 using var _ = watch.Watch<V1Namespace, V1NamespaceList>(
                     onEvent: (type, item) =>
                     {
@@ -454,6 +700,44 @@ namespace ArgoCD.Client.Test.Utilities
             }
         }
 
+#if NET5_0 || NETSTANDARD2_1
+        private async Task DeleteResourceAsync(IKubernetesObject<V1ObjectMeta> objectMeta, string fieldSelector)
+        {
+            switch (objectMeta.Kind)
+            {
+                case V1CustomResourceDefinition.KubeKind:
+                    var definitionList =
+                        await kubernetes.ListCustomResourceDefinitionAsync(null, null, fieldSelector,
+                            null);
+                    if (definitionList.Items.Any())
+                    {
+                        _ = await kubernetes.DeleteCustomResourceDefinitionAsync(objectMeta.Name());
+                    }
+
+                    break;
+
+                case V1ClusterRole.KubeKind:
+                    var clusterRoleList =
+                        await kubernetes.ListClusterRoleAsync(null, null, fieldSelector);
+                    if (clusterRoleList.Items.Any())
+                    {
+                        _ = await kubernetes.DeleteClusterRoleAsync(objectMeta.Name());
+                    }
+
+                    break;
+
+                case V1ClusterRoleBinding.KubeKind:
+                    var clusterRoleBindingList =
+                        await kubernetes.ListClusterRoleBindingAsync(null, null, fieldSelector);
+                    if (clusterRoleBindingList.Items.Any())
+                    {
+                        _ = await kubernetes.DeleteClusterRoleBindingAsync(objectMeta.Name());
+                    }
+
+                    break;
+            }
+        }
+#else
         private async Task DeleteResourceAsync(IKubernetesObject<V1ObjectMeta> objectMeta, string fieldSelector)
         {
             switch (objectMeta.Kind)
@@ -490,15 +774,23 @@ namespace ArgoCD.Client.Test.Utilities
                     break;
             }
         }
+#endif
 
         private async Task CreateNameSpaceAsync()
         {
             try
             {
-                await kubernetes.CoreV1.CreateNamespaceAsync(new V1Namespace()
+#if NET5_0 || NETSTANDARD2_1
+                await kubernetes.CreateNamespaceAsync(new V1Namespace()
                 {
                     Metadata = new V1ObjectMeta() { Name = NameSpace }
                 });
+#else
+    await kubernetes.CoreV1.CreateNamespaceAsync(new V1Namespace()
+                {
+                    Metadata = new V1ObjectMeta() { Name = NameSpace }
+                });
+#endif
             }
             catch (Exception ex)
             {
