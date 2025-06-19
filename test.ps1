dcba4a71ebbb6bfa366d3093450f073fc190b23a
huhouhua <huhouhuam@outlook.com>
2025-06-19 16:04:53 +0800

    test: add build

--- test.ps1
+++ test.ps1
@@ -1,9 +1,10 @@
 $dockerOSType = docker info --format "{{.OSType}}"
 if ($dockerOSType -ieq 'linux') {
-  & dotnet test  --verbosity normal --settings coverletArgs.runsettings --no-build   --no-restore
+  & dotnet test  --verbosity normal --settings coverletArgs.runsettings 
 } else {
-  & dotnet test --filter Category!=LinuxIntegration --verbosity normal --settings coverletArgs.runsettings  --no-build --no-restore
+  & dotnet test --filter Category!=LinuxIntegration --verbosity normal --settings coverletArgs.runsettings 
 }
 if ($LastExitCode -ne 0) {
   exit 1
 }
+#--no-build   --no-restore
