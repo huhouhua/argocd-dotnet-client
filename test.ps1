38eec426d987fe22170c0787cc7964b7b37ad52c
huhouhua <huhouhuam@outlook.com>
2025-06-19 15:54:30 +0800

    ci: fix upload coverage

--- test.ps1
+++ test.ps1
@@ -4,16 +4,6 @@ if ($dockerOSType -ieq 'linux') {
 } else {
   & dotnet test --filter Category!=LinuxIntegration --verbosity normal --settings coverletArgs.runsettings  --no-build --no-restore
 }
-
-$exitWithError = $LastExitCode -ne 0
-
-$openCoverFile = Get-ChildItem -Path "test/*/coverage.opencover.xml" -Recurse | Sort-Object LastWriteTime | Select-Object -last 1
-if (Test-Path "$openCoverFile") {
-  Write-Host "Uploading coverage file"
-  & codecov -f "$openCoverFile" -t $env:CODECOV_TOKEN
-  $exitWithError = $exitWithError -or $LastExitCode -ne 0
-}
-
-if ($exitWithError) {
+if ($LastExitCode -ne 0) {
   exit 1
 }
