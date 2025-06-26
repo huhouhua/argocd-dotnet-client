5c6168b36486ea7d82c66bddaa3b2c7c32039169
huhouhua <huhouhuam@outlook.com>
2025-06-26 20:46:37 +0800

    fix: add gpg options for create

---
+++ src/ArgoCD.Client/Internal/Builders/GPGListQueryBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using ArgoCD.Client.Internal.Utilities;
+using ArgoCD.Client.Models.GPKKey.Requests;
+
+
+namespace ArgoCD.Client.Internal.Builders
+{
+    internal class GPGListQueryBuilder : QueryBuilder<GPGListQueryOptions>
+    {
+        protected override void BuildCore(Query query, GPGListQueryOptions options)
+        {
+            if (options.KeyID.IsNotNullOrEmpty())
+            {
+                query.Add("keyID", options.KeyID);
+            }
+        }
+    }
+}
---
+++ src/ArgoCD.Client/Models/GPGKey/Requests/GPGListQueryOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace ArgoCD.Client.Models.GPKKey.Requests;
+
+public sealed class GPGListQueryOptions
+{
+    public GPGListQueryOptions()
+    {
+
+    }
+
+    /// <summary>
+    /// The GPG key ID to query for.
+    /// </summary>
+    public string KeyID { get; set; }
+}
